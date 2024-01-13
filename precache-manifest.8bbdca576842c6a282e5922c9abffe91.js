494a26d53279a87a2bda2daa2daa295a37673d9f
wongella123 <wongella123@gmail.com>
2024-01-13 13:59:49 +0800

    Updates

--- precache-manifest.8bbdca576842c6a282e5922c9abffe91.js
+++ precache-manifest.8bbdca576842c6a282e5922c9abffe91.js
@@ -1,82 +1,82 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7b7e34096388978e0a1161d0fb22491",
-    "url": "./index.html"
+    "revision": "7efae211b96e8b9d2b9d433e333c9496",
+    "url": "/index.html"
   },
   {
     "revision": "9a915be014aba4366867",
-    "url": "./static/css/main.ec349ec6.chunk.css"
+    "url": "/static/css/main.ec349ec6.chunk.css"
   },
   {
     "revision": "9af66c765832ef1e941b",
-    "url": "./static/js/2.a6ca760e.chunk.js"
+    "url": "/static/js/2.a6ca760e.chunk.js"
   },
   {
     "revision": "6a2a939c6c472481a460",
-    "url": "./static/js/3.5a11cc32.chunk.js"
+    "url": "/static/js/3.5a11cc32.chunk.js"
   },
   {
     "revision": "9a915be014aba4366867",
-    "url": "./static/js/main.3f12da20.chunk.js"
+    "url": "/static/js/main.3f12da20.chunk.js"
   },
   {
-    "revision": "680febfa5999cdd8a38c",
-    "url": "./static/js/runtime~main.be903308.js"
+    "revision": "6d57d2952c711127ca6e",
+    "url": "/static/js/runtime~main.93c88974.js"
   },
   {
     "revision": "023e2a28f6d4793153bee5861438c745",
-    "url": "./static/media/My_Grade.023e2a28.xlsx"
+    "url": "/static/media/My_Grade.023e2a28.xlsx"
   },
   {
     "revision": "ce817ff9f2894736a36a1dbc44e9187c",
-    "url": "./static/media/My_image.ce817ff9.JPG"
+    "url": "/static/media/My_image.ce817ff9.JPG"
   },
   {
     "revision": "d3fc8f6e4f718e4a2fbd2149e7cfc8a8",
-    "url": "./static/media/ShootingGame.d3fc8f6e.png"
+    "url": "/static/media/ShootingGame.d3fc8f6e.png"
   },
   {
     "revision": "6a536511c01bff7e552b8e329b98b2d2",
-    "url": "./static/media/ToDoList.6a536511.png"
+    "url": "/static/media/ToDoList.6a536511.png"
   },
   {
     "revision": "5e5c20b431a54551e2b2b4616b09544b",
-    "url": "./static/media/USElection.5e5c20b4.png"
+    "url": "/static/media/USElection.5e5c20b4.png"
   },
   {
     "revision": "e79de56da7d0b6a43d55d413fd2d2af4",
-    "url": "./static/media/cake2.e79de56d.png"
+    "url": "/static/media/cake2.e79de56d.png"
   },
   {
     "revision": "44f91641ef71fe58c5997ea497309089",
-    "url": "./static/media/extractFileInfo.44f91641.png"
+    "url": "/static/media/extractFileInfo.44f91641.png"
   },
   {
     "revision": "1c5c0a8ba8a67e7b027f44e5e248741a",
-    "url": "./static/media/life1.1c5c0a8b.JPG"
+    "url": "/static/media/life1.1c5c0a8b.JPG"
   },
   {
     "revision": "1fbff41f4d1895d826861e3a191e9176",
-    "url": "./static/media/life2.1fbff41f.png"
+    "url": "/static/media/life2.1fbff41f.png"
   },
   {
     "revision": "a178e0907f915a64ec06ebaa856e837f",
-    "url": "./static/media/life3.a178e090.jpg"
+    "url": "/static/media/life3.a178e090.jpg"
   },
   {
     "revision": "9f7722f7c2979dd01acb453bea7b70dd",
-    "url": "./static/media/life4.9f7722f7.jpg"
+    "url": "/static/media/life4.9f7722f7.jpg"
   },
   {
     "revision": "4d5b85f2f7639008efb5f6f354f13229",
-    "url": "./static/media/life5.4d5b85f2.jpg"
+    "url": "/static/media/life5.4d5b85f2.jpg"
   },
   {
     "revision": "e201b1024b24b3e839b4f6dd56596018",
-    "url": "./static/media/life6.e201b102.JPG"
+    "url": "/static/media/life6.e201b102.JPG"
   },
   {
     "revision": "9c8a0ab1802629206a4c177eb0b66cbf",
-    "url": "./static/media/tictactoe.9c8a0ab1.png"
+    "url": "/static/media/tictactoe.9c8a0ab1.png"
   }
 ]);
