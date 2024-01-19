05e3f01da925a9d747de121de1bdfe26853d3642
wongella123 <wongella123@gmail.com>
2024-01-19 21:39:38 +0800

    change some contents

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -18,15 +18,21 @@ const Intro = () => {
         <img className="intro-image" src={My_image} alt="My_image" />
         <div className="intro-content">
           <h2 className={`welcome ${animate ? "animate-class" : ""}`}>
-            Hello, I am Ella Wong from HKUST
+            Hello, I am Ella Wong!
           </h2>
+          <p>
+            I am currently pursuing a degree in{" "}
+            <strong>Information Systems + Artificial Intelligence</strong> at
+            the Hong Kong University of Science and Technology.
+          </p>
+
           <div style={{ display: "flex", justifyContent: "center" }}>
             <div className="pointing-emoji">👉</div>
             <button
               className="intro-button"
               onClick={() =>
                 window.open(
-                  "https://drive.google.com/file/d/1jkcj9pPnGy538OfsaqRzvc5WuhOHTN1b/view?usp=sharing"
+                  "https://drive.google.com/file/d/1pEZnKZFBkdTtzdWsZoG51rSx5JMGhfIS/view?usp=sharing"
                 )
               }
             >
